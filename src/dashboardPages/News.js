1c50a63cf6ca102c412b9c6d9d5563613d132360
PhilipNzube <Onwubaliliphilip@gmail.com>
2024-10-25 23:56:44 +0100

    Updated

--- src/dashboardPages/News.js
+++ src/dashboardPages/News.js
@@ -1,35 +1,86 @@
 import NewsImg from '../images/NewsImg.png'
 import NewsCard from '../components/NewsCard'
+import InfoImg from '../images/ep_info-filled.png'
+import BottomInfo from '../components/BottomInfo'
+import FireImg from '../images/Fire.png'
 
 export default function News() {
     return (
         <>
             <div id="NewsMain">
                 <div id="NewsMainSub">
-                <div id="NewsMainSubText">
-                Featured News
+                    <div id="NewsMainSubText">
+                        Featured News
                     </div>
                     <div id="NewsMainSub2">
-                    <NewsCard
-                        content="This place here is the headline of the news related to SK."
-                        newstag="Fox News"
-                        time="2:00 PM"
-                        img={NewsImg}
-                    />
-                    <NewsCard
-                        content="This place here is the headline of the news related to SK."
-                        newstag="CNN News"
-                        time="2:00 PM"
-                        img={NewsImg}
-                    />
-                    <NewsCard
-                        content="This place here is the headline of the news related to SK."
-                        newstag="BBC News"
-                        time="2:00 PM"
-                        img={NewsImg}
+                        <NewsCard
+                            content="This place here is the headline of the news related to SK."
+                            newstag="Fox News"
+                            time="2:00 PM"
+                            img={NewsImg}
                         />
+                        <NewsCard
+                            content="This place here is the headline of the news related to SK."
+                            newstag="CNN News"
+                            time="2:00 PM"
+                            img={NewsImg}
+                        />
+                        <NewsCard
+                            content="This place here is the headline of the news related to SK."
+                            newstag="BBC News"
+                            time="2:00 PM"
+                            img={NewsImg}
+                        />
+                    </div>
+                    <div id="NewsMainSubText">
+                        Today’s Headlines
+                    </div>
+                    <div id="NewsMainSub3">
+
+                    </div>
+                    <div id="TopLeadersNews">
+                        <div id="TLMainSub">
+                            <div id="TLText">
+                                Top Leaders
+                                <img id="FireImg" src={FireImg} alt="FireImg" />
+                                <img id="InfoImg" src={InfoImg} alt="InfoImg" />
+                            </div>
+                        </div>
+                        <div id="TLSubAlign">
+                            <div id="TLSub">
+                                <div id="SubHeader">Rank</div>
+                                <div>1</div>
+                                <div>2</div>
+                                <div>3</div>
+                                <div>4</div>
+                                <div>5</div>
+                            </div>
+
+
+                            <div id="TLSub2">
+                                <div id="SubHeader">Name</div>
+                                <div>Abdul</div>
+                                <div>Philip</div>
+                                <div>Sarmie</div>
+                                <div>Ivy</div>
+                                <div>Vicks</div>
+                            </div>
+
+
+                            <div id="TLSub3">
+                                <div id="SubHeader">Points</div>
+                                <div>25</div>
+                                <div>20</div>
+                                <div>15</div>
+                                <div>10</div>
+                                <div>5</div>
+                            </div>
+                        </div>
+                        <div id="TLMainSub">
+                            <BottomInfo content="This Week" redarrow="none" />
                         </div>
                     </div>
+                </div>
             </div>
         </>
     )
