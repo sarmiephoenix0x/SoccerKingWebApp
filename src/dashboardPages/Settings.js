439ea3993c3f0c46a82347b4dbf756551f8da9ef
PhilipNzube <Onwubaliliphilip@gmail.com>
2024-10-26 22:03:39 +0100

    Update

--- src/dashboardPages/Settings.js
+++ src/dashboardPages/Settings.js
@@ -5,32 +5,34 @@ export default function Settings() {
     return (
         <>
             <div id="SettingsMain">
+                <div id="Profile">
                 <div id="SettingsSpacer">
-                <div id="SettingsHeader">
-                    Your Profile
+                    <div id="SettingsHeader">
+                        Your Profile
+                    </div>
+                    <div id="SettingsAshText">
+                        Joined 2/6/23
+                    </div>
                 </div>
-                <div id="SettingsAshText">
-                    Joined 2/6/23
-                    </div>
-                    </div>
                 <img id="PlayerProfileImg2" src={PlayerProfileImg} alt="PlayerProfileImg" />
                 <div id="SettingsSpacer">
-                <div id="SettingsColumnizer">
-                <div id="SettingsHeader">
-                Soccer, Kingdom
-                </div>
-                <div id="SettingsAshText">
-                +234 1234567890
-                </div>
-                </div>
-                {/* <div id="ReduceSize"> */}
-                <div id="SettingsCurvedColoredItem">
-                <img id="EditImg" src={EditImg} alt="EditImg" />
-                Edit
+                    <div id="SettingsColumnizer">
+                        <div id="SettingsHeader">
+                            Soccer, Kingdom
+                        </div>
+                        <div id="SettingsAshText">
+                            +234 1234567890
+                        </div>
+                    </div>
+                    {/* <div id="ReduceSize"> */}
+                    <div id="SettingsCurvedColoredItem">
+                        <img id="EditImg" src={EditImg} alt="EditImg" />
+                        Edit
                     </div>
                     {/* </div> */}
-                    </div>
-            </div>
+                </div>
+                </div>
+                </div>
         </>
     )
 }
