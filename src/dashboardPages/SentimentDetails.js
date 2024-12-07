7c7aa82a3b90d03177d207ecc6a32b3862ed800d
PhilipNzube <Onwubaliliphilip@gmail.com>
2024-12-07 01:57:08 +0100

    `Refactored SentimentDetails.js to improve layout and styling`

--- src/dashboardPages/SentimentDetails.js
+++ src/dashboardPages/SentimentDetails.js
@@ -42,82 +42,95 @@ export default function SentimentDetails() {
             }}>
                 {/* TradingView Chart */}
                 <TradingViewPage isHeaderActive={false} />
-                <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                <Box sx={{ marginTop: 2 }}>
-                <Typography variant="h5" sx={{ marginTop: 3, fontWeight: 'bold' }}>
-                    Share Your Opinion
-                    </Typography>
-                </Box>
-                
-                <Box sx={{ marginTop: 2 }}>
-                <Typography variant="h6" sx={{ marginTop: 3 }}>
-                {sentiment.title} will go
-                    </Typography>
-                    </Box>
-                {/* UP/DOWN Tabs */}
-                <Box sx={{ marginTop: 2 }}>
-                    <Tabs value={voteTabIndex} onChange={handleVoteTabChange} indicatorColor="success">
-                        <Tab label="UP" sx={{ color: 'white' }} />
-                        <Tab label="DOWN" sx={{ color: 'white' }} />
-                    </Tabs>
-                </Box>
-                <Box sx={{ marginTop: 2 }}>
-                <Typography variant="h6" sx={{ marginTop: 3 }}>
-                {sentiment.title} will go UP over the
-                    </Typography>
-                    </Box>
-                {/* Time Period Tabs */}
-                <Box sx={{ marginTop: 2 }}>
-                    <Tabs value={timePeriodTabIndex} onChange={handleTimePeriodTabChange} indicatorColor="primary">
-                        <Tab label="DAY" sx={{ color: 'white' }} />
-                        <Tab label="WEEK" sx={{ color: 'white' }} />
-                        <Tab label="MONTH" sx={{ color: 'white' }} />
-                    </Tabs>
-                </Box>
+                <Box sx={{ marginX: { xs: 4, sm: 4 } }}>
+                    <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                        <Box sx={{ marginTop: 2 }}>
+                            <Typography variant="h5" sx={{ marginTop: 3, fontWeight: 'bold' }}>
+                                Share Your Opinion
+                            </Typography>
+                        </Box>
 
-                {/* Display sentiment details */}
-                <Box sx={{ display: 'flex', alignItems: 'center', marginTop: 3 }}>
-                    <Typography variant="h6" sx={{ marginRight: 2 }}>
-                        Notify me when the prediction has finished
-                    </Typography>
-                    <Switch
-                        checked={notify}
-                        onChange={handleNotifyChange}
-                        color="primary"
-                        sx={{
-                            '&.Mui-checked': {
-                                color: 'green', // Green color when active
-                            },
-                            '&.Mui-checked + .MuiSwitch-track': {
-                                backgroundColor: 'green', // Background color when active
-                            },
-                            '& .MuiSwitch-track': {
-                                backgroundColor: '#b0bec5', // Grey background when inactive
-                            },
-                        }}
-                    />
-                    </Box>
-                    <Box sx={{ display: 'flex', alignItems: 'center', marginTop: 6, width: '100%' }}>
-                    <Button
-      variant="contained"
+                        <Box sx={{ marginTop: 2 }}>
+                            <Typography variant="h6" sx={{ marginTop: 3 }}>
+                                {sentiment.title} will go
+                            </Typography>
+                        </Box>
+                        {/* UP/DOWN Tabs */}
+                        <Box sx={{ marginTop: 2 }}>
+                            <Tabs value={voteTabIndex} onChange={handleVoteTabChange} indicatorColor="success">
+                                <Tab label="UP" sx={{ color: 'white' }} />
+                                <Tab label="DOWN" sx={{ color: 'white' }} />
+                            </Tabs>
+                        </Box>
+                        <Box sx={{ marginTop: 2 }}>
+                            <Typography variant="h6" sx={{ marginTop: 3 }}>
+                                {sentiment.title} will go UP over the
+                            </Typography>
+                        </Box>
+                        {/* Time Period Tabs */}
+                        <Box sx={{ marginTop: 2 }}>
+                            <Tabs value={timePeriodTabIndex} onChange={handleTimePeriodTabChange} indicatorColor="primary">
+                                <Tab label="DAY" sx={{ color: 'white' }} />
+                                <Tab label="WEEK" sx={{ color: 'white' }} />
+                                <Tab label="MONTH" sx={{ color: 'white' }} />
+                            </Tabs>
+                        </Box>
+
+                        {/* Display sentiment details */}
+                        <Box sx={{ display: 'flex', alignItems: 'center', marginTop: 3 }}>
+                            <Typography variant="h6" sx={{ marginRight: 2 }}>
+                                Notify me when the prediction has finished
+                            </Typography>
+                            <Switch
+                                checked={notify}
+                                onChange={handleNotifyChange}
+                                color="primary"
+                                sx={{
+                                    '&.Mui-checked': {
+                                        color: 'green', // Green color when active
+                                    },
+                                    '&.Mui-checked + .MuiSwitch-track': {
+                                        backgroundColor: 'green', // Background color when active
+                                    },
+                                    '& .MuiSwitch-track': {
+                                        backgroundColor: '#b0bec5', // Grey background when inactive
+                                    },
+                                }}
+                            />
+                        </Box>
+                        <Box
                             sx={{
-                                marginX: 50,
-        backgroundColor: '#FCE74F', // Custom background color
-          color: '#000', 
-        '&:hover': {
-            backgroundColor: '#FFD700', 
-            boxShadow: '0px 4px 20px rgba(0, 0, 0, 0.3)',
-                                },
-                                boxShadow: 'none',
+                                display: 'flex',
+                                alignItems: 'center',
+                                justifyContent: 'center', // Center the button
+                                marginTop: 6,
+                                width: '100%',
+                                paddingX: { xs: 2, sm: 4 }, // Add padding for smaller screens
                             }}
-                            fullWidth
-    >
-      Share
-    </Button>
+                        >
+                            <Button
+                                variant="contained"
+                                sx={{
+                                    backgroundColor: '#FCE74F', // Custom background color
+                                    color: '#000',
+                                    '&:hover': {
+                                        backgroundColor: '#FFD700',
+                                        boxShadow: '0px 4px 20px rgba(0, 0, 0, 0.3)',
+                                    },
+                                    boxShadow: 'none',
+                                    width: '100%', // Ensure the button takes full width
+                                    maxWidth: 400, // Optional: Set a max width for larger screens
+                                    marginX: { xs: 0, sm: 50 }, // Adjust margin based on screen size
+                                }}
+                                fullWidth
+                            >
+                                Share
+                            </Button>
+                        </Box>
+                        {/* Add more sentiment details here */}
                     </Box>
-                {/* Add more sentiment details here */}
                 </Box>
-                </Box>
+            </Box>
         </>
     );
 }
